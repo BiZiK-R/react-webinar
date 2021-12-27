1a424dcdf14b5695da6ba5307a5dd57d6a44d566
Andrey Gorbunov <bizik2000@gmail.com>
2021-12-27 09:18:33 +0300

    fix: merged waiting, fix textarea, moved the initialization for the form to the store, fix select

--- src/app/article-edit/index.js
+++ src/app/article-edit/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useCallback} from "react";
 import Layout from "../../components/layout";
 import useStore from "../../utils/use-store";
 import useSelector from "../../utils/use-selector";
@@ -23,32 +23,17 @@ function ArticleEdit() {
   }, [params.id]);
 
   const select = useSelector(state => ({
-    waitingArticle: state.article.waiting,
-    waitingCategory: state.category.waiting,
-    waitingCountry: state.country.waiting,
-    waitingForm: state.formArticle.waiting,
+    waiting: state.article.waiting || state.category.waiting || state.country.waiting || state.formArticle.waiting,
     article: state.article.data,
   }));
 
   useEffect(() => {
-    store.formArticle.initData({
-      title: select.article.title ? select.article.title : '',
-      description: select.article.description ? select.article.description : '',
-      maidIn: select.article.maidIn ? select.article.maidIn._id : '',
-      category: select.article.category ? select.article.category._id : '',
-      edition: select.article.edition ? select.article.edition : '',
-      price: select.article.price ? select.article.price : '',
-      id: select.article._id ? select.article._id : '',
-    });
+    store.formArticle.initData(select.article);
   }, [select.article]);
 
-  const onSubmit = async () => {
-    await store.formArticle.put();
-    await store.get('article').load(params.id);
-  }
-
-  const onDelete = () => {
-    store.formArticle.del();
+  const callbacks = {
+    onPut: useCallback(() => store.formArticle.put(), [store]),
+    onDelete: useCallback(() => store.formArticle.del(), [store]),
   }
 
   return (
@@ -56,10 +41,10 @@ function ArticleEdit() {
 
       <Header/>
 
-      <Spinner active={select.waitingArticle || select.waitingCategory || select.waitingCountry || select.waitingForm}>
+      <Spinner active={select.waiting}>
         <WrapperContent>
-          <FormArticle onSubmit={onSubmit} />
-          <button type="button" onClick={onDelete}>Удалить товар</button>
+          <FormArticle onSubmit={callbacks.onPut} />
+          <button type="button" onClick={callbacks.onDelete}>Удалить товар</button>
         </WrapperContent>
       </Spinner>
     </Layout>
--- src/containers/catalog-filter/index.js
+++ src/containers/catalog-filter/index.js
@@ -41,10 +41,10 @@ function CatalogFilter() {
 
   return (
     <LayoutTools>
-      <Select onChange={(e) => callbacks.onCategory(e.target.value)} value={select.selectCategory} options={options.category}/>
+      <Select onChange={callbacks.onCategory} value={select.selectCategory} options={options.category}/>
       <Input onChange={callbacks.onSearch} value={select.query} placeholder={'Поиск'} theme="big"/>
       <label>Сортировка:</label>
-      <Select onChange={(e) => callbacks.onSort(e.target.value)} value={select.sort} options={options.sort}/>
+      <Select onChange={callbacks.onSort} value={select.sort} options={options.sort}/>
       <button onClick={callbacks.onReset}>Сбросить</button>
     </LayoutTools>
   );
